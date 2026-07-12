d51e783ff1bf58d41aff72f06a017b59308a2b70
noomenbm <noomen.belmechri@gmail.com>
2026-07-12 00:42:13 -0600

    feat: add application shell and routing

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'events/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'book/:eventId',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'events'
+  },
+  {
+    path: 'events',
+    loadComponent: () =>
+      import('./pages/events/events').then((component) => component.Events)
+  },
+  {
+    path: 'events/:id',
+    loadComponent: () =>
+      import('./pages/event-details/event-details').then(
+        (component) => component.EventDetails
+      )
+  },
+  {
+    path: 'book/:eventId',
+    loadComponent: () =>
+      import('./pages/booking/booking').then((component) => component.Booking)
+  },
+  {
+    path: 'bookings',
+    loadComponent: () =>
+      import('./pages/my-bookings/my-bookings').then(
+        (component) => component.MyBookings
+      )
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./pages/not-found/not-found').then((component) => component.NotFound)
+  }
+];
